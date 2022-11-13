5777159c757063eddc6eba0c5b480b2389f20190
elinalak <lakida_v@icloud.com>
2022-11-13 01:12:29 +0100

    Resolved exercise 1.6

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,12 +1,29 @@
 import { useState } from "react";
 
+const Header = (props) => <h2>{props.name}</h2>;
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <Header name="give feedback" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Header name="statistics" />
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
 };
 
 export default App;
